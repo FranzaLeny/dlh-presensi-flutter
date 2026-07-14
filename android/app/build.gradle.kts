f38e5aacf391c457bfcc5d862bbc6143bd123a33
franza <fransaleny49@gmail.com>
2026-07-14 14:34:21 +0800

    feat: use dynamic applicationId with .github suffix on CI and bump version to 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "id.go.dlh.presensi_dlh"
+        val appIdSuffix = System.getenv("APP_ID_SUFFIX") ?: ""
+        applicationId = "id.go.dlh.presensi_dlh" + appIdSuffix
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
